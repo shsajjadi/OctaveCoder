40de717099234ba86ccaeac7cb56d5b81c98fc10
shsajjadi <sajjadi.floss@gmail.com>
2026-05-31 18:06:12 +0330

    show args of mkoctfile for dbg purpose

--- src/build_system.cpp
+++ src/build_system.cpp
@@ -591,6 +591,7 @@ namespace coder_compiler
 
     auto call_mkoctfile = [&] (const std::string& args)
     {
+      std::cout << args << "\n";
       static const std::string mkoctfile_exe = find_mkoctfile ();
 
       octave_value_list ret = OCTAVE_DEPR_NS Fsystem (octave_value (quote (mkoctfile_exe) + " " + args), 2);
